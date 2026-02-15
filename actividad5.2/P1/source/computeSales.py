ca4be8d8cd799b6ed3f10882962981871c5919ce
Oliver Martinez <olivermartinez@MacBook-Air-de-Oliver.local>
2026-02-15 16:49:09 -0600

    Se guardan los resultados en archivos de salida

--- actividad5.2/P1/source/computeSales.py
+++ actividad5.2/P1/source/computeSales.py
@@ -20,10 +20,11 @@ def main():
     file_name_sales = sys.argv[2]
     filepath_catalogue = '../tests/' + file_name_catalogue
     filepath_sales = '../tests/' + file_name_sales
+    lines = []
 
+    print("Catalogue file: ", file_name_catalogue)
+    print("Sales file: ", file_name_sales)
     print("Reading files...")
-    print("Catalogue file: ", filepath_catalogue)
-    print("Sales file: ", filepath_sales)
 
     #leer el catálogo de productos
     with open (filepath_catalogue, 'r', encoding="utf-8") as file:
@@ -44,7 +45,22 @@ def main():
             if product['title'] == product_sale:
                 total_sales += product['price'] * quantity
 
-    print(f"Total Sales: {total_sales}")
+
+
+    lines.append("-----------------------")
+    lines.append(f"Total Sales: ${total_sales:,.2f}")
+    lines.append("-----------------------\n")
+
+    #Definir el nombre del archivo de resultados
+    archivo_salida = '../results/SalesResults_' + file_name_sales.replace('.json', '.txt')
+
+    # Escribir las líneas en un archivo de resultados
+    with open(archivo_salida, "w", encoding="utf-8") as file:
+        for line in lines:
+            print(line)
+            file.write(line + "\n")
+    file.close()
+
 
 
 if __name__ == "__main__":

dcf8ce7b7fad558f7751e1cf1ec18b44630541c4
Oliver Martinez <olivermartinez@MacBook-Air-de-Oliver.local>
2026-02-15 17:38:42 -0600

    Agregamos evidencia de ejecuciones y evidencia de pylint

--- actividad5.2/P1/source/computeSales.py
+++ actividad5.2/P1/source/computeSales.py
@@ -24,21 +24,33 @@ def main():
 
     print("Catalogue file: ", file_name_catalogue)
     print("Sales file: ", file_name_sales)
-    print("Reading files...")
-    print("-------------------\n")
+    print("Reading files...\n")
     start_time = time.perf_counter()
 
     #leer el catálogo de productos
-    with open (filepath_catalogue, 'r', encoding="utf-8") as file:
-        catalogue = json.load(file)
+    try:
+        with open (filepath_catalogue, 'r', encoding="utf-8") as file:
+            catalogue = json.load(file)
+    except FileNotFoundError:
+        print(f"Error: File '{filepath_catalogue}' not found.")
+        return
+    except json.JSONDecodeError:
+        print(f"Error: Invalid JSON in file '{filepath_catalogue}'.")
+        return
 
     #leer las ventas
-    with open (filepath_sales, 'r', encoding="utf-8") as file:
-        sales = json.load(file)
+    try:
+        with open (filepath_sales, 'r', encoding="utf-8") as file:
+            sales = json.load(file)
+    except FileNotFoundError:
+        print(f"Error: File '{filepath_sales}' not found.")
+        return
+    except json.JSONDecodeError:
+        print(f"Error: Invalid JSON in file '{filepath_sales}'.")
+        return
 
     #Creamos un diccionario con la información del catálogo para facilitar la búsqueda de precios
     catalogue_dict = {product['title']: product['price'] for product in catalogue}
-    
 
     #Recorrer las ventas y calcular el total de ventas
     total_sales = 0
@@ -49,19 +61,26 @@ def main():
 
         if product_sale in catalogue_dict:
             total_sales += catalogue_dict[product_sale] * quantity
-            lines.append(f"{product_sale} \n Unit Price: ${catalogue_dict[product_sale]} \n Quantity: {quantity} \n Total: ${catalogue_dict[product_sale] * quantity} \n")
+            lines.append(
+                f"{product_sale}\n"
+                f"Unit Price: ${catalogue_dict[product_sale]}\n"
+                f"Quantity: {quantity}\n"
+                f"Total: ${catalogue_dict[product_sale] * quantity}\n"
+            )
         else:
-            lines.append(f"{product_sale} \n Unit Price: PRODUCT NOT FOUND IN CATALOGUE \n Quantity: {quantity} \n Total: NOT CALCULATED \n")
-                
+            lines.append(
+                f"{product_sale}\n"
+                f"PRODUCT NOT FOUND IN CATALOGUE\n"
+                f"Quantity: {quantity}\n"
+                )
 
     end_time = time.perf_counter()
-    elapsed_time = end_time - start_time
 
 
     lines.append("-----------------------")
     lines.append(f"Total Sales: ${total_sales:,.2f}")
     lines.append("-----------------------\n")
-    lines.append(f"Execution Time: {elapsed_time:.6f} seconds")
+    lines.append(f"Execution Time: {end_time - start_time:.6f} seconds")
 
     #Definir el nombre del archivo de resultados
     archivo_salida = '../results/SalesResults_' + file_name_sales.replace('.json', '.txt')
@@ -69,9 +88,17 @@ def main():
     # Escribir las líneas en un archivo de resultados
     with open(archivo_salida, "w", encoding="utf-8") as file:
         for line in lines:
-            print(line)
+            #print(line)
             file.write(line + "\n")
     file.close()
+
+    print(
+        f"Detailed information file: {archivo_salida}\n"
+        f"-----------------------\n"
+        f"Total Sales: ${total_sales:,.2f}\n"
+        f"-----------------------\n"
+        f"Execution Time: {end_time - start_time:.6f} seconds\n"
+    )
 
 
 

0b604bf882380d3ef1a7dd76a5480e1817e2dce9
Oliver Martinez <olivermartinez@MacBook-Air-de-Oliver.local>
2026-02-20 19:59:13 -0600

    Se agrega clase Entity para metodos abstractos Create y Display Info

--- actividad6.2/P1/source/reservation_system.py
+++ actividad6.2/P1/source/reservation_system.py
@@ -1,4 +1,5 @@
 import sys
+from abc import ABC, abstractmethod
 
 
 #Creamos los diccionarios para almacenar la informacion de los clientes y las reservas
@@ -7,47 +8,65 @@ reservations_dict = {}
 hotels_dict = {}
 
 
+#Clase abstracta para definir la estructura de las clases de clientes, hoteles y reservas
+class Entity(ABC):
+    @abstractmethod
+    def create(self, *args):
+        pass
+
+    @abstractmethod
+    def display_info(self):
+        pass
+
+
 #Clase para crear clientes
-class Customers:
-    def create(email, name, birthdate, phone):
-        customers_dict[email] = {
+class Customers(Entity):
+
+    def create(self, email, name, birthdate, phone):
+        self.email = email
+
+        customers_dict[self.email] = {
             "name": name,
             "birthdate": birthdate,
             "phone": phone
         }
         print(f"Customer '{name}' created successfully.")
 
-    def display_info(email):
-        customer = customers_dict.get(email)
+    def display_info(self):
+        customer = customers_dict.get(self.email)
         if customer:
             print(f"Customer Name: {customer['name']}")
-            print(f"Email: {email}")
+            print(f"Email: {self.email}")
             print(f"Birthdate: {customer['birthdate']}")
             print(f"Phone: {customer['phone']}")
         else:
             print("Customer not found.")
 
+
 #Clase para crear hoteles
-class Hotels:
-    def create(name, location, price_per_night):
+class Hotels(Entity):
+
+    def create(self, name, location, price_per_night):
+        self.name = name
+
         hotels_dict[name] = {
             "location": location,
             "price_per_night": price_per_night
         }
         print(f"Hotel '{name}' created successfully.")
 
-    def display_info(name):
-        hotel = hotels_dict.get(name)
+    def display_info(self):
+        hotel = hotels_dict.get(self.name)
         if hotel:
-            print(f"Hotel Name: {name}")
+            print(f"Hotel Name: {self.name}")
             print(f"Location: {hotel['location']}")
             print(f"Price per Night: ${hotel['price_per_night']}")
         else:
             print("Hotel not found.")
 
 #Clase para crear reservas
-class Reservations:
-    def create(customer_email, hotel_name, check_in_date, check_out_date):
+class Reservations(Entity):
+    def create(self, customer_email, hotel_name, check_in_date, check_out_date):
         if customer_email not in customers_dict:
             print("Customer not found. Please create the customer first.")
             return
@@ -64,16 +83,20 @@ class Reservations:
         }
         print(f"Reservation '{reservation_id}' created successfully.")
 
-    def display_info(reservation_id):
-        reservation = reservations_dict.get(reservation_id)
+        self.reservation_id = reservation_id
+
+    def display_info(self):
+        reservation = reservations_dict.get(self.reservation_id)
         if reservation:
-            print(f"Reservation ID: {reservation_id}")
+            print(f"Reservation ID: {self.reservation_id}")
             print(f"Customer Email: {reservation['customer_email']}")
             print(f"Hotel Name: {reservation['hotel_name']}")
             print(f"Check-in Date: {reservation['check_in_date']}")
             print(f"Check-out Date: {reservation['check_out_date']}")
         else:
             print("Reservation not found.")
+
+
 
     
 
